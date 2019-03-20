3b9d5355dcd4e5607d546a6e7125ca0f90bc86fa
Ssshake Laptop <ssshake@laptop>
2019-03-19 21:10:20 -0400

    manually transcribed alexs work

--- src/game.js
+++ src/game.js
@@ -1,29 +1,90 @@
 export default class Game {
-    constructor(ctx, grid, framesPerSecond){
+    constructor(ctx, colors, timer){
         this.ctx = ctx;
+        this.colors = colors;
+        this.timer = timer;
+        this.handle = null;
+        this.cursor = null;
+    }
+
+    start(grid) {
         this.grid = grid;
-        this.framesPerSecond = framesPerSecond;
+        this.cellSize = [
+            this.ctx.canvas.width / this.grid.width,
+            this.ctx.canvas.height / this.grid.height,
+        ]
+        this.queue();
     }
 
-    generation() {
-        this.ctx.clearRect(0, 0, this.grid.fieldSize, this.grid.fieldSize)
-
-        drawGrid(this.ctx, this.grid)
-        
-        const gridOfNextGeneration = getNextGeneration(this.grid)
-        
-        setTimeout(() => {
-        
-            requestAnimationFrame(() => generation(this.ctx, gridOfNextGeneration))
-        
-        }, 
-        1000 / this.framesPerSecond)
+    stop() {
+        this.timer.clear(this.hanle);
+        this.handle = null;
     }
 
+    queue(){
+        this.handle = this.timer.trigger( () => this.tick() );
+    }
 
+    setCursor(cursorTuple) {
+        this.cursor = cursorTuple.map((value, axis) => {
+            return Math.floor ( value, this.cellSize[axis] * this.cellSize[axis] )
+        });
+    }
 
+    removeCursor() {
+        this.cursor = null;
+    }
 
+    erase() {
+        this.ctx.fillStyle = this.colors.clear;
+        this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    }
 
+    draw() {
+        this.ctx.strokeStyle = this.colors.cellStroke;
+        this.ctx.fillStyle = this.colors.cellFill;
 
-    
+        const dostuff = (cell, {row, column}) => {
+            if (!cell.isAlive()){
+                return;
+            }
+
+            this.ctx.fillStyle = cell.color();
+
+            const rect = [
+                column * this.cellSize[0],
+                row * this.cellSize[1], //perhaps an object instead, this.cellSize.x or row
+                this.cellSize[0],
+                this.cellSize[1],
+            ];
+
+            this.ctx.fillRect(...rect);
+            this.ctx.strokeRect(...rect);
+        };
+        
+        this.grid.eachCell(dostuff);
+    }
+
+    drawCursor() {
+        if (!this.cursor) {
+            return;
+        }
+
+        this.ctx.fillStyle = "#FF00FF";
+        this.ctx.fillRect(
+            this.cursor[0],
+            this.cursor[1],
+            this.cellSize[0],
+            this.cellSize[1],
+
+        );
+    }
+
+    tick() {
+        this.erase();
+        this.grid.update();
+        this.draw();
+        this.drawCursor();
+        this.queue();
+    }
 }
