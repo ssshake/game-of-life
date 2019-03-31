1a425c413ba5eb39e1ca0e4cfe5f916cc20114f8
Richard Bettridge <r.bettridge@vehikl.com>
2019-03-31 19:55:40 -0400

    screensaver for mac

---
+++ screensaver/osx/Web.saver/Contents/Resources/cell.js
@@ -0,0 +1,66 @@
+const neighboursMatrix = [
+    [-1, 0, 1],
+    [-1, 0, 1],
+    [-1, 0, 1],
+];
+
+//value.Clamp(0,1).toInt().
+const normalizeValue = v => (v > 0 ? 1 : 0);
+
+export default class Cell {
+    constructor(grid, column, row, value = 0) {
+        this.hsl = [0, '100%', '70%']
+    
+        this.meta = {
+          continuity: 0,
+        };
+        this._value = normalizeValue(value);
+        this._position = {
+          row,
+          column
+        };
+    
+        const wrap = (size, v) => v < 0 ?
+          v + size :
+          (v >= size ?
+            v - size :
+            v);
+    
+        this._neighbours = neighboursMatrix.reduce((neighbours, matrixRow, r) => {
+          return matrixRow.reduce((nextNeighbours, cell, c) => {
+            if (r === 1 && c === 1) return nextNeighbours;
+    
+            return nextNeighbours.concat({
+              column: wrap(grid.width, column + (c - 1)),
+              row: wrap(grid.height, row + (r - 1)),
+            });
+          }, neighbours);
+        }, []);
+    }
+
+    color () {
+        return `hsl(${this.hsl.join(', ')})`;
+    }
+
+    isAlive() {
+        return this._value === 1;
+    }
+
+    getNeighbours(grid){
+        return this._neighbours.map((position) => {
+            return grid.getCell(position.column, position.row);
+        });
+    }
+
+    get neighbours (){
+        return this._neighbours;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(value){
+        this._value = normalizeValue(value);
+    }
+}
---
+++ screensaver/osx/Web.saver/Contents/Resources/grid.js
@@ -0,0 +1,84 @@
+const makeArrayOfSize = (size) => {
+    return 'x'.repeat(size).split('').map(() => null);
+}
+
+import Cell from './cell.js';
+
+export default class Grid {
+    constructor(width, height){
+        this.width = width;
+        this.height = height || width;
+        this.emptyGrid()
+    }
+
+    emptyGrid() {
+
+        this.cells = makeArrayOfSize(this.height).map((_, row) => {
+                return makeArrayOfSize(this.width).map((_, column) => {
+                    return new Cell(this, column, row, 0)
+                });
+            });
+    }
+
+    seed( rareness =  1 / 2 ) {
+        this.eachCell((cell) => {
+            cell.value = Math.random() > ( 1.0 - rareness) ? 1 : 0;
+        })
+    }
+
+    eachCell(callback){
+        this.cells.forEach((rowCells, row) => {
+            rowCells.forEach((cell, column) => {
+                callback(cell, {
+                    row,
+                    column
+                });
+            });
+        });
+    }
+
+    getCell(column, row) {
+        return this.cells[row] && this.cells[row][column];
+    }
+
+
+    update(){
+        let commits = [];
+
+        const dostuff = (cell, { row, column} ) => {
+            const neighbours = cell.getNeighbours(this);
+
+            const sum = ( arr, get) => {
+                 return arr.reduce((sum, item) => {
+                    return sum + get(item)
+                }, 0);
+            };
+
+            const count = sum(neighbours, c => c.value);
+
+            if (cell.value === 0 && count === 3 ){ //spawn
+                const aliveNeighbours = neighbours.filter((c) => {
+                    return c.isAlive();
+                })
+
+                commits.push([cell, 1]);
+                // cell.meta.continuity +=1;
+                // cell.hsl[0] = ((
+                //     sum(aliveNeighbours, c => c.hsl[0]) / aliveNeighbours.length
+                // ) + 10) % 360;
+            } else if ( (cell.value ===1 ) && (count != 2 && count != 3)) { //die
+                commits.push([cell, 0]);
+                cell.meta.continuity = 0;
+                cell.hsl[0] = 0;
+                // cell.hsl[0] = cell.meta.continuity % 360;
+            } else { //live
+                cell.meta.continuity +=1;
+                cell.hsl[0] = cell.meta.continuity % 360;
+            }           
+
+        };
+
+        this.eachCell(dostuff);
+        commits.forEach(([cell, value]) => { cell.value = value });
+    }  
+}
